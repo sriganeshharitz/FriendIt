cf9419e1c6bc40c2650a4163983c18c349cb9379
Sriganesh Nagaraj <sriganeshharitz@gmail.com>
2017-09-12 17:45:11 +0530

    first commit

---
+++ src/main/java/com/uttara/project/FriendIt/FriendItApplication.java
@@ -0,0 +1,12 @@
+package com.uttara.project.FriendIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendItApplication.class, args);
+	}
+}
---
+++ src/test/java/com/uttara/project/FriendIt/FriendItApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uttara.project.FriendIt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FriendItApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
